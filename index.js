be06a8c2c0a5dd1bba71b1c31164a74ff0e57d4e
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-15 15:17:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  gameInterval = null
+  clearInterval(gameInterval)
   for (var i = 0; i < ROCKS.LENGTH; i++) {
     ROCKS[i].pop()
   }
